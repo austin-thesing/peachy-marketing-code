d0dc287977791a8a53d220401e2b68ec1fc1b98b
Austin Thesing <austin@designxdevelop.com>
2022-03-08 18:17:01 -0700

    Target page title

--- theme-switcher.js
+++ theme-switcher.js
@@ -17,7 +17,7 @@
   	return getCookie("theme");
   };
   
-
+  //Check for existing cookie/theme setting
   $(document).ready(function(){
     console.log("Hey!");
     if(checkForWeb2()){
@@ -25,7 +25,7 @@
     	  $("div.tag-early-access").addClass("web2");
         $(".navlink").addClass("web2");
       	
-      //Home - Theme Setup
+      //Home Page - Theme Setup
         $(".tag-coming-soon").addClass("web2");
         $(".headline").addClass("web2");
         $(".paint-swipe").addClass("web2");
@@ -33,10 +33,11 @@
         $(".button").addClass("web2");
         $(".col-slider-w-bg-shape").addClass("web2");
       
-     //Resource - Theme Setup
+     //Resource Page - Theme Setup
         $(".btn-scrolldown").addClass("web2");
         $(".btn-text").addClass("web2");
         $(".arrow-icon").addClass("web2");
+        $(".page-intro---title").addClass("web2");
     }    
    });
   
@@ -45,7 +46,7 @@
     	//Remove Cookie
     	console.log("Remove Cookie");
     	document.cookie = "theme=; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
-    	//document.cookie = 'theme=; Max-Age=0; path=/; domain=' + location.host;
+
 
     } else {
     	//Set Cookie
@@ -53,13 +54,16 @@
     	setCookie();
     }
       
-//Home Page - Theme Targets
+//Nav + Global - Theme Targets
     $("div.tag-early-access").toggleClass("web2");
     $(".navlink").toggleClass("web2");
+    $(".powered-by---tag").toggleClass("web2");
+
+//Home Page - Theme Targets
     $(".tag-coming-soon").toggleClass("web2");
     $(".headline").toggleClass("web2");
     $(".paint-swipe").toggleClass("web2");
-    $(".powered-by---tag").toggleClass("web2");
+    
     $(".button").toggleClass("web2");
     $(".col-slider-w-bg-shape").toggleClass("web2");
     
@@ -67,5 +71,6 @@
     $(".btn-scrolldown").toggleClass("web2");
     $(".btn-text").toggleClass("web2");
     $(".arrow-icon").toggleClass("web2");
+    $(".page-intro---title").toggleClass("web2");
     
   });   
