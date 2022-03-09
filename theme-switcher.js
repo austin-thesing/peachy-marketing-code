6bf9bf903c2c87018b3dfc6cfb9afd4b68ff7d86
Austin Thesing <austin@designxdevelop.com>
2022-03-08 18:25:00 -0700

    Update

--- theme-switcher.js
+++ theme-switcher.js
@@ -1,4 +1,4 @@
-// use to deliver from repo via CDN https://www.jsdelivr.com/github
+// use this to deliver script via CDN https://www.jsdelivr.com/github
   
   function getCookie(name) {
     let cookie = {};
